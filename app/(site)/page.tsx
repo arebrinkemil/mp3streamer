f7c61bd6fac4398649b51af8a04c2ae2a76d32e8
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-08-08 11:27:59 +0200

    homepage

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,10 +8,20 @@ export default function Home() {
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem />
+            <ListItem
+              image="/images/liked.png"
+              name="liked songs"
+              href="Liked"
+            />
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
+        </div>
+        <div>Lost of songs</div>
+      </div>
     </div>
   );
 }
