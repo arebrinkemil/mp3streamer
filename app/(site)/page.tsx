3166447b67144d2a893590067bdddd4f078c0740
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-08-18 01:12:34 +0200

    scroll area + style

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,6 +3,7 @@ import { Header } from '@/components/Header';
 import { ListItem } from '@/components/ListItem';
 import { PageContent } from './components/PageContent';
 import { Player } from '@/components/Player';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 import {
   Accordion,
@@ -19,14 +20,14 @@ export default async function Home() {
   return (
     <div className="bg-neutral-900 rounded-lg h-[inherit] w-full overflow-hidden overflow-y-hidden flex align-bottom">
       <Accordion type="single" defaultValue="item-1" className="w-full">
-        <AccordionItem value="item-1">
+        <AccordionItem className="bg-[#3f5efb] rounded-lg" value="item-1">
           <AccordionTrigger>
             {' '}
             <Header>
               <div className=""></div>
             </Header>
           </AccordionTrigger>
-          <AccordionContent className="pb-0">
+          <AccordionContent className="pb-0 bg-[#eeeeee] rounded-lg">
             <div className="h-[calc(100vh-(366px+1rem))]">
               <div className="">
                 <h1 className="text-white text-3xl text-semibold">Welcome back</h1>
@@ -37,12 +38,12 @@ export default async function Home() {
             </div>
           </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-2">
+        <AccordionItem className="bg-[#3f5efb] rounded-lg" value="item-2">
           <AccordionTrigger>
             <div className="text-white h-[90px]">playlist</div>
           </AccordionTrigger>
-          <AccordionContent className="pb-0">
-            <div className="  px-6 h-[calc(100vh-(366px+1rem))] overflow-y-scroll">
+          <AccordionContent className="pb-0 bg-[#eeeeee] rounded-lg">
+            <ScrollArea className="px-6 h-[calc(100vh-(366px+1rem))]">
               <div className="flex justify-between items-center">
                 <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
               </div>
@@ -51,29 +52,17 @@ export default async function Home() {
 
                 {/* {songs.map((song) => <div>{song.title}</div>)} */}
               </div>
-              <div>
-                <PageContent songs={songs} />
-
-                {/* {songs.map((song) => <div>{song.title}</div>)} */}
-              </div>
-              <div>
-                <PageContent songs={songs} />
-
-                {/* {songs.map((song) => <div>{song.title}</div>)} */}
-              </div>
-              <div>
-                <PageContent songs={songs} />
-
-                {/* {songs.map((song) => <div>{song.title}</div>)} */}
-              </div>
-            </div>
+            </ScrollArea>
           </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-3">
+        <AccordionItem
+          className="bg-[radial-gradient(circle,_rgba(253,77,30,1)_0%,_rgba(63,94,251,1)_74%)] rounded-lg"
+          value="item-3"
+        >
           <AccordionTrigger>
             <Player />
           </AccordionTrigger>
-          <AccordionContent className="pb-0">
+          <AccordionContent className="pb-0 bg-[#eeeeee] rounded-lg">
             <div className="h-[calc(100vh-(366px+1rem))]">
               <PageContent songs={songs} />
             </div>
